2051ea78f94cb7114d98af9d39e254913273321f
Charles Driver <cdriver.aus@gmail.com>
2021-08-02 14:37:50 +0200

    .

--- testing/algoserver.R
+++
@@ -1,6 +0,0 @@
-svSocket::startSocketServer(8888);
-require(bigIRT);
-setwd("/home/driver/bigIRT/testing/")
-items <- read.csv(file = 'items.csv')
-persons = read.csv(file='persons.csv')
-while (!exists("done")) Sys.sleep(1)
